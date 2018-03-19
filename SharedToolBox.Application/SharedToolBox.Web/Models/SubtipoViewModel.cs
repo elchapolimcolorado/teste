c82e5f7bb2fd07b67063f1f7b2ee3235a85b539e
Thiago Augusto Souza <elchapolimcolorado@hotmail.com>
2018-03-19 00:24:55 -0300

    inclusao

---
+++ SharedToolBox.Application/SharedToolBox.Web/Models/SubtipoViewModel.cs
@@ -0,0 +1,44 @@
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SharedToolBox.Web.Models
+{
+    public class SubtipoViewModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Codigo { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo Nome")]
+        [MaxLength(150, ErrorMessage = "Máximo {0} caracteres")]
+        [MinLength(2, ErrorMessage = "Minimo {0} caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Selecione uma imagem")]
+        [DisplayName("Imagem")]
+        public byte[] Imagem { get; set; }
+
+        [DisplayName("NomeArquivo")]
+        public string NomeArquivo { get; set; }
+
+        [DisplayName("ContentType")]
+        public string ContentType { get; set; }
+
+        [DisplayName("Tipo")]
+        public TipoViewModel Tipo { get; set; }
+
+        [DisplayName("CodigoTipo")]
+        public int CodigoTipo { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime DataManipulacao { get; set; }
+
+        [ScaffoldColumn(false)]
+        public bool Ativo { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string LoginManipulacao { get; set; }
+    }
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
